8952d3f94a6f48ce90668778256c9773e68fb7f0
Keerthana <keerthanaravi1133@gmail.com>
2024-09-10 16:18:19 +0530

    Divisible by 3 or 5.
    Given an integer value, if it is divisible by 3 print “HI”, if it is divisible by 5 print “HELLO”. If it is divisible by both print “HIHELLO”, else  print "NONE"

---
+++ Divisible by 3 or 5.java
@@ -0,0 +1,24 @@
+import java.util.*;
+class Main{
+  public static void main(String[] args){
+    Scanner sc=new Scanner(System.in);
+    long a=sc.nextLong();
+    if(a%3==0){
+      if(a%5==0){
+        System.out.println("HIHELLO");
+      }
+      else{
+        System.out.println("HI");
+      }
+    }
+    if(a%5==0){
+      if(a%3==0){
+        System.out.println("HIHELLO");
+      }
+      else{
+        System.out.println("HELLO");
+ 
+  }
+}
+}
+}
